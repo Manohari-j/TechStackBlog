270eabfe2183601ae22c07d9dac4ea7abc72f990
Manohari Jayachandran <110393522+Manohari-j@users.noreply.github.com>
2026-06-12 08:44:25 -0500

    Update Program.cs https://www.techstackblog.com

--- TechBlog/backend/Program.cs
+++ TechBlog/backend/Program.cs
@@ -21,7 +21,7 @@ builder.Services.AddCors(options => {
     options.AddPolicy("AllowFrontend", policy =>
         policy.WithOrigins(
             "http://localhost:3000",
-            "https://www.techstackblog.com/"
+            "https://www.techstackblog.com"
         )
         .AllowAnyMethod()
         .AllowAnyHeader()
